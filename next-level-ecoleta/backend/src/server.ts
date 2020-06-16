34189b4ff1bf34b9f8bd9b15b906b68296653fbc
Diego França <diego.franca92@gmail.com>
2020-06-16 15:52:46 -0300

    inicio do backend

--- next-level-ecoleta/backend/src/server.ts
+++ next-level-ecoleta/backend/src/server.ts
@@ -1,16 +1,34 @@
 import express from 'express';
 
 const app = express();
+const users = [
+    "01",
+    "12",
+    "03",
+    "14"
+];
 
 app.get('/users', (request, response) =>{
-    console.log('Listagem de Usuários');
+   const search = String(request.query.search)
+
+   const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
     
-    response.json([
-        "1",
-        "2",
-        "3",
-        "4"
-    ]);
+   return response.json(filteredUsers);
 });
+
+app.get('/users/:id', (request, response) =>{
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    return response.json(user);
+ });
+
+ app.get('/users', (request, response) =>{
+    const user = {
+        name: 'Diego',
+        email: 'diego@gmail.com'
+    }
+    return response.json(user);
+ });
 
 app.listen(3333);
