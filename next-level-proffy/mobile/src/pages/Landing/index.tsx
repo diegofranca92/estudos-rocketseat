ff96f5c82837b2cc5310e21b458787add506b645
Diego França <diego.franca92@gmail.com>
2020-10-14 11:11:14 -0300

    atualizações de Fontes e Estilos

--- next-level-proffy/mobile/src/pages/Landing/index.tsx
+++ next-level-proffy/mobile/src/pages/Landing/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text } from 'react-native';
 
 import styles from "./styles";
 import LandingIMG from "../../assets/images/landing.png";
 
 function Landing() {
     return (
-      <View>
-          <Image source={LandingIMG} />
+      <View style={styles.container}>
+          <Image style={styles.banner} source={LandingIMG} />
+          <Text style={styles.title}>
+            Seja bem-vindo, {'\n'}
+            <Text style={styles.titleBold}>O que deseja fazer?</Text>
+          </Text>
       </View>
     );
 }
--- next-level-proffy/mobile/src/pages/Landing/styles.ts
+++ next-level-proffy/mobile/src/pages/Landing/styles.ts
@@ -1,7 +1,25 @@
 import { StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-
+    container:{
+        flex: 1,
+        backgroundColor: '#8257E5',
+        justifyContent: 'center',
+        padding: 40
+    },
+    banner: {
+        width: '100%',
+        resizeMode: 'contain'
+    },
+    title:{
+        color: '#fff',
+        fontSize: 20,
+        lineHeight: 30,
+        marginTop: 80
+    },
+    titleBold:{
+        fontWeight: 'bold'
+    }
 })
 
 export default styles;
